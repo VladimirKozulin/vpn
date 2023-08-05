256f67179a2531169c3f4817bfb5ce14a26c8b6d
VladimirKozulin <vladimir.kozulin.18@yandex.ru>
2023-08-05 09:47:33 +0300

    Add VPN configuration properties

--- src/main/java/com/example/vpn/config/VpnProperties.java
+++ src/main/java/com/example/vpn/config/VpnProperties.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Data
 @Component
 @ConfigurationProperties(prefix = "vpn")
@@ -20,4 +22,32 @@ public class VpnProperties {
     
     // Путь к конфигурационному файлу Xray
     private String configPath;
+    
+    // Reality настройки
+    private Reality reality = new Reality();
+    
+    @Data
+    public static class Reality {
+        // Включить Reality протокол
+        private boolean enabled = true;
+        
+        // Целевой сайт для маскировки (например "www.microsoft.com:443")
+        private String dest = "www.microsoft.com:443";
+        
+        // Список SNI (Server Name Indication)
+        private List<String> serverNames = List.of("www.microsoft.com");
+        
+        // Приватный ключ (генерируется автоматически если пустой)
+        private String privateKey = "";
+        
+        // Публичный ключ (генерируется автоматически если пустой)
+        private String publicKey = "";
+        
+        // Список shortIds (пустая строка = можно подключаться без shortId)
+        private List<String> shortIds = List.of("", "6ba85179e30d4fc2");
+        
+        // uTLS fingerprint
+        private String fingerprint = "chrome";
+    }
 }
+

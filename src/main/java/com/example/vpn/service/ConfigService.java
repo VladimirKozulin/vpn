31b557ea3bfbfd13104f2f2b1d3096030fbea339
VladimirKozulin <vladimir.kozulin.18@yandex.ru>
2023-10-03 11:56:21 +0300

    Update VLESS link generation with Reality parameters

--- src/main/java/com/example/vpn/service/ConfigService.java
+++ src/main/java/com/example/vpn/service/ConfigService.java
@@ -6,9 +6,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 /**
  * Сервис для генерации конфигурации клиента
- * Создает ссылки для подключения к VPN
+ * Создает ссылки для подключения к VPN с поддержкой Reality
  */
 @Slf4j
 @Service
@@ -20,6 +24,9 @@ public class ConfigService {
     /**
      * Генерирует VLESS ссылку для конкретного клиента
      * Формат: vless://UUID@SERVER:PORT?параметры#название
+     * 
+     * С Reality: vless://UUID@IP:443?encryption=none&flow=xtls-rprx-vision&security=reality
+     *            &pbk=PUBLIC_KEY&fp=chrome&sni=www.microsoft.com&sid=SHORT_ID&type=tcp#NAME
      */
     public String generateVlessLink(VpnClient client) {
         // Базовая часть: vless://UUID@адрес:порт
@@ -30,15 +37,50 @@ public class ConfigService {
         );
         
         // Параметры подключения
-        String params = "?encryption=none&type=tcp&security=none";
+        StringBuilder params = new StringBuilder("?encryption=none&type=tcp");
+        
+        if (vpnProperties.getReality().isEnabled()) {
+            // Reality параметры
+            params.append("&security=reality");
+            params.append("&flow=xtls-rprx-vision");
+            params.append("&pbk=").append(vpnProperties.getReality().getPublicKey());
+            params.append("&fp=").append(vpnProperties.getReality().getFingerprint());
+            
+            // SNI (Server Name Indication)
+            if (!vpnProperties.getReality().getServerNames().isEmpty()) {
+                String sni = vpnProperties.getReality().getServerNames().get(0);
+                params.append("&sni=").append(sni);
+            }
+            
+            // ShortId (берём первый непустой или пустой)
+            String shortId = vpnProperties.getReality().getShortIds().stream()
+                .filter(id -> !id.isEmpty())
+                .findFirst()
+                .orElse("");
+            if (!shortId.isEmpty()) {
+                params.append("&sid=").append(shortId);
+            }
+            
+            log.info("✅ Сгенерирована VLESS+Reality ссылка для клиента UUID: {}", client.getUuid());
+        } else {
+            // Без Reality (небезопасно!)
+            params.append("&security=none");
+            log.warn("⚠️ Сгенерирована VLESS ссылка БЕЗ Reality для клиента UUID: {}", client.getUuid());
+        }
         
         // Название подключения (будет отображаться в клиенте)
-        String name = "#" + (client.getDeviceInfo() != null ? 
-            client.getDeviceInfo() : "VPN-Client");
+        String name = client.getDeviceInfo() != null ? 
+            client.getDeviceInfo() : "VPN-Client";
         
-        String vlessLink = base + params + name;
+        try {
+            name = URLEncoder.encode(name, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            log.warn("Не удалось закодировать имя клиента: {}", name);
+        }
         
-        log.info("Сгенерирована VLESS ссылка для клиента UUID: {}", client.getUuid());
+        String vlessLink = base + params.toString() + "#" + name;
+        
+        log.debug("VLESS ссылка: {}", vlessLink);
         return vlessLink;
     }
 }
